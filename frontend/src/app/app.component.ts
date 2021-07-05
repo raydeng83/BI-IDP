c0af217c2218c03b0394efb18d662e9070b60eb5
raydeng83 <ray.deng83@gmail.com>
2021-07-04 23:23:21 -0400

    add frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Licensed under the Single Application / Multi Application License.
+ * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
+ */
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from './@core/utils';
+
+@Component({
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent implements OnInit {
+
+  constructor(private analytics: AnalyticsService) {
+  }
+
+  ngOnInit(): void {
+    this.analytics.trackPageViews();
+  }
+}
